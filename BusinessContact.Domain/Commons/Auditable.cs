0820669a4cef0541eb43338540a7340d80dd431b
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-22 01:18:18 +0500

    Changed Auditable

--- BusinessContact.Domain/Commons/Auditable.cs
+++ BusinessContact.Domain/Commons/Auditable.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessContact.Domain.Commons
+﻿namespace BusinessContact.Domain.Commons
 {
     public class Auditable
     {
         public long Id { get; set; }
-        public string FirstName { get; set; }
-        public string? LastName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string? DateOfBirth { get; set; }
-        public DateTime CreatedAt { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? LastUpdatedAt { get; set; }
+        public long DeletedBy { get; set; }
+        public long UpdatedBy { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
