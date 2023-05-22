0f12def892f09730c970103405c101d373749e0c
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-22 13:32:24 +0500

    Fixed auditable

--- BusinessContact.Domain/Commons/Auditable.cs
+++ BusinessContact.Domain/Commons/Auditable.cs
@@ -5,8 +5,8 @@
         public long Id { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? LastUpdatedAt { get; set; }
-        public long DeletedBy { get; set; }
-        public long UpdatedBy { get; set; }
+        public long? DeletedBy { get; set; }
+        public long? UpdatedBy { get; set; }
         public bool IsDeleted { get; set; }
     }
 }
