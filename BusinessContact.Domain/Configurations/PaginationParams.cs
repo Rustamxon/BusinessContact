8105e28246c481f1da9ac42c8ec4b944ab9bf5ff
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-22 14:02:08 +0500

    Added PaginationParams to Domain layer

---
+++ BusinessContact.Domain/Configurations/PaginationParams.cs
@@ -0,0 +1,20 @@
+﻿namespace BusinessContact.Domain.Configurations
+{
+    public class PaginationParams
+    {
+        private int pageSize;
+        private int pageIndex;
+
+        public int PageSize
+        {
+            get => pageSize;
+            set => pageSize = value <= 0 ? 20 : value;
+        }
+
+        public int PageIndex
+        {
+            get => pageIndex;
+            set => pageIndex = value <= 0 ? 1 : value;
+        }
+    }
+}
