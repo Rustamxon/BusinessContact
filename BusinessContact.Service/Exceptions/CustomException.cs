54181a700fb72f4a132f994b0619044c43a83367
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-23 23:32:53 +0500

    Added exceptions

---
+++ BusinessContact.Service/Exceptions/CustomException.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessContact.Service.Exceptions;
+
+public class CustomException : Exception
+{
+    public int Code { get; set; }
+    public CustomException(int code, string Message) : base(Message)
+    {
+        this.Code = code;
+    }
+}
