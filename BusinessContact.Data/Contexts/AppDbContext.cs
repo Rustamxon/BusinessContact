2cf97edeb9b2ea4550b143fc58801c981c46a6b1
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-23 23:19:37 +0500

    Added context and install ef core tools and sqlserver

---
+++ BusinessContact.Data/Contexts/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BusinessContact.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessContact.Data.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<UserImage> UserImages { get; set; }
+    }
+}
