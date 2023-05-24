f4fba8042e555bda6d25ea7edaebf5635dcd1aee
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 12:41:02 +0500

    Complete collection extension

--- BusinessContact.Service/Extensions/CollectionExtension.cs
+++ BusinessContact.Service/Extensions/CollectionExtension.cs
@@ -1,6 +1,26 @@
-﻿namespace BusinessContact.Service.Extensions
+﻿using BusinessContact.Domain.Commons;
+using BusinessContact.Domain.Configurations;
+using BusinessContact.Service.Exceptions;
+
+namespace BusinessContact.Service.Extensions
 {
-    internal class CollectionExtension
+    public static class CollectionExtension
     {
+        public static IQueryable<TEntity> ToPagedList<TEntity>(this IQueryable<TEntity> entities, PaginationParams @params)
+        where TEntity : Auditable
+        {
+            return @params.PageIndex > 0 && @params.PageSize > 0 ?
+                entities.OrderBy(e => e.Id)
+                    .Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize) :
+                        throw new CustomException(400, "Please, enter valid numbers");
+        }
+        public static IEnumerable<TEntity> ToPagedList<TEntity>(this IEnumerable<TEntity> entities, PaginationParams @params)
+            where TEntity : Auditable
+        {
+            return @params.PageIndex > 0 && @params.PageSize > 0 ?
+                entities.OrderBy(e => e.Id)
+                    .Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize) :
+                        throw new CustomException(400, "Please, enter valid numbers");
+        }
     }
 }
