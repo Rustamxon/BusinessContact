45abe6d7476c5085825913858f3ff8d9aa3844f5
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 12:43:05 +0500

    Complete common extension

--- BusinessContact.Service/Extensions/CommonExtension.cs
+++ BusinessContact.Service/Extensions/CommonExtension.cs
@@ -1,6 +1,16 @@
-﻿namespace BusinessContact.Service.Extensions
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BusinessContact.Service.Extensions
 {
-    internal class CommonExtension
+    public static class CommonExtension
     {
+        public static byte[] ToByteArray(this IFormFile file)
+        {
+            using var memoryStream = new MemoryStream();
+            using var fileStream = file.OpenReadStream();
+            fileStream.Seek(0, SeekOrigin.Begin);
+            fileStream.CopyTo(memoryStream);
+            return memoryStream.ToArray();
+        }
     }
 }
