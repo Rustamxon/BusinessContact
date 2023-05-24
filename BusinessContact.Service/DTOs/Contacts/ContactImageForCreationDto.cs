d348c26856eba96b7991e735c40b0c4d1da58ae0
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 12:21:22 +0500

    Added contact image for result dto and fixed contact image for creation dto

--- BusinessContact.Service/DTOs/Contacts/ContactImageForCreationDto.cs
+++ BusinessContact.Service/DTOs/Contacts/ContactImageForCreationDto.cs
@@ -5,6 +5,6 @@ namespace BusinessContact.Service.DTOs.Contacts
     public class ContactImageForCreationDto
     {
         public IFormFile Image { get; set; }
-        public long UserId { get; set; }
+        public long ContactId { get; set; }
     }
 }
---
+++ BusinessContact.Service/DTOs/Contacts/ContactImageForResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessContact.Service.DTOs.Contacts
+{
+    public class ContactImageForResultDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public long ContactId { get; set; }
+    }
+}
