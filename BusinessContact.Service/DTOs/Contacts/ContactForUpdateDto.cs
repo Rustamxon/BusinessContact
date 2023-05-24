952e40fcf9b0ed0cd55783919f8d6141c6788c33
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 05:16:25 +0500

    Added contact for update dto

---
+++ BusinessContact.Service/DTOs/Contacts/ContactForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessContact.Service.DTOs.Contacts
+{
+    public class ContactForUpdateDto
+    {
+        [Required(ErrorMessage = "FirstName is required")]
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
