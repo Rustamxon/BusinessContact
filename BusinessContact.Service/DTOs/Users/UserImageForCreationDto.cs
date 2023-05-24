b2088cab4a6656e0eb5a24c3ca315f1f8f69c69a
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 12:09:42 +0500

    Added user image for creation dto

---
+++ BusinessContact.Service/DTOs/Users/UserImageForCreationDto.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace BusinessContact.Service.DTOs.Users
+{
+    public class UserImageForCreationDto
+    {
+        public IFormFile Image { get; set; }
+        public int UserId { get; set; }
+    }
+}
