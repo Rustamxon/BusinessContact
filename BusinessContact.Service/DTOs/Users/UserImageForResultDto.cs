590b99f8e6e938f2ebf5601ee49316493850cff0
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 12:11:57 +0500

    Added userimage for result dto

---
+++ BusinessContact.Service/DTOs/Users/UserImageForResultDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BusinessContact.Service.DTOs.Users
+{
+    public class UserImageForResultDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public long UserId { get; set; }
+    }
+}
