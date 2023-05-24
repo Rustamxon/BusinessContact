22b2ea7c4c9861064274a8f5bb647205abc5b065
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 11:33:07 +0500

    Complete user for creation dto

--- BusinessContact.Service/DTOs/Contacts/ContactForResultDto.cs
+++ BusinessContact.Service/DTOs/Contacts/ContactForResultDto.cs
@@ -4,6 +4,7 @@ namespace BusinessContact.Service.DTOs.Contacts
 {
     public class ContactForResultDto
     {
+        public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone { get; set; }
--- BusinessContact.Service/DTOs/Users/UserForCreationDto.cs
+++ BusinessContact.Service/DTOs/Users/UserForCreationDto.cs
@@ -1,10 +1,25 @@
-﻿namespace BusinessContact.Service.DTOs.Users
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessContact.Service.DTOs.Users
 {
     public class UserForCreationDto
     {
+        [Required(ErrorMessage = "FirstName is required")]
         public string FirstName { get; set; }
+
         public string LastName { get; set; }
+
+        
+        [Required(ErrorMessage = "Phone number is required")]
         public string PhoneNumber { get; set; }
+
+        
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
         public string Email { get; set; }
+        
+        
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
     }
 }
