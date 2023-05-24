d9b4510930648661ab2116c65a76a230a2140672
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 05:12:41 +0500

    Added contact for result dto

---
+++ BusinessContact.Service/DTOs/Contacts/ContactForResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessContact.Service.DTOs.Contacts
+{
+    public class ContactForResultDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
