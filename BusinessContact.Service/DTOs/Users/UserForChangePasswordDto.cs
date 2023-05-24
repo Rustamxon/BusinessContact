b1a9f5915d5e976192901021dbc87f47bd7a9af9
Rustamxon Rahimov <rahimovr872@gmail.com>
2023-05-24 11:55:44 +0500

    Added user for change password dto

---
+++ BusinessContact.Service/DTOs/Users/UserForChangePasswordDto.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BusinessContact.Service.DTOs.Users
+{
+    public class UserForChangePasswordDto
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Old password must not be null or empty")]
+        public string OldPassword { get; set; }
+
+
+        [Required(ErrorMessage = "New password must not be null or empty")]
+        public string NewPassword { get; set; }
+
+
+        [Required(ErrorMessage = "New password must not be null or empty")]
+        [Compare("NewPassword")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
